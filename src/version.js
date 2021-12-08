e1f3ccd2058c823d2084da5aeedb7b8df98e019b
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-12-07 23:36:18 -0600

    Create version.js

---
+++ src/version.js
@@ -0,0 +1,17 @@
+const version = (prefix, pushname) => {
+    return `
+    *DestroyerBot ву Juan del Valle*
+
+    *Actualizado:* 07 de Diciembre del 2021
+    *Versión actual:* 10.1
+    *Edición ofrecida por:* Juan del Valle™
+
+
+
+
+_*by Juan del Valle*_
+`
+
+}
+
+exports.version = version
