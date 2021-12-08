b07661bcf945e7788195967c23504356ba7ab770
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2021-12-07 23:37:38 -0600

    Create help.js

---
+++ src/help.js
@@ -0,0 +1,102 @@
+const help = (prefix) => {
+	return `
+
+⌜ _*DestroyerBot by Juan Del Valle*_ ⌟  
+
+◉ *INFORMACION*
+   ○ Comando: ⌜ ${prefix} ⌟
+   ○ Editor: Juan Del Valle 
+
+◉ *COMUNICADO*
+Si en dado caso algún comando fallase,favor de escribirme por privado para tratar de solucionar el problema
+
+◉ *PARA USAR EL BOT*
+Registrate con el comando ${prefix}registro y tu nombre
+
+◉ *NUEVOS COMANDOS*
+○ ${prefix}banmenu
+Banear a personas el uso del bot
+○ ${prefix}levelmenu
+Level para tus grupos
+○ ${prefix}antimenu
+Su nombre lo dice todo (antilink)
+○ ${prefix}otak
+Monas chinas
+○ ${prefix}shantera
+Interactua con el bot
+
+◉ *NUEVOS MENUS*
+○ ${prefix}juegos
+Diviértete con tus amigos :)
+○ ${prefix}desmenu
+Descargar musica y videos
+○ ${prefix}version
+Conoce la versión de tu bot
+○ ${prefix}welmenu
+Comando de bienvenida solo grupos
+
+◉ *RESUELVE TUS DUDAS*
+  ║
+  ╠ ○ ${prefix}creador
+  ╚ Dudas o problemas aqui
+
+◉ *CREAR STICKERS*
+  ║
+  ╠ ○ ${prefix}sticker
+  ╠ ○ ${prefix}attp
+  ╠ Mas un texto corto
+  ╠ ○ ${prefix}stickergif
+  ╚ 6 segundos de video
+
+◉ *CONVERTIDORES*
+  ║
+  ╠ ○ ${prefix}toimg
+  ╠ De sticker a JPG
+  ╠ ○ ${prefix}tomp3
+  ╚ De video a MP3
+
+◉ *AUDIO*
+  ║
+  ╠ ○ ${prefix}idioma
+  ╚ ○ ${prefix}tts es (mas texto)
+
+◉ *OTROS*
+  ║
+  ╠ ○ ${prefix}wame
+  ╠ Link de Whatsapp
+  ╠ ○ ${prefix}qrcode
+  ╚ Coloca un texto
+
+◉ *GRUPOS*
+  ║
+  ╠ ○ ${prefix}unir
+  ╠ Unir a una persona al grupo
+  ╠ ○ ${prefix}fgc
+  ╠ Cambiar foto del grupo
+  ╠ ○ ${prefix}ngc
+  ╠ Cambiar nombre del grupo
+  ╠ ○ ${prefix}dgc
+  ╠ Cambiar descripcion del grupo
+  ╠ ○ ${prefix}closegc
+  ╠ Cerrar el grupo solo admins
+  ╠ ○ ${prefix}opengc
+  ╠ Abrir grupo solo admins
+  ╠ ○ ${prefix}promote
+  ╠ Dar admin a un miembro
+  ╠ ○ ${prefix}demote
+  ╠ Quitar el admin
+  ╠ ○ ${prefix}linkgc
+  ╠ Link del grupo
+  ╠ ○ ${prefix}adminlist
+  ╠ Nombra a los admins del grupo
+  ╠ ○ ${prefix}todos
+  ╚ Nombra a todos los del grupo
+
+Para usar estas funciones el bot necesita admin
+  
+
+_*by Juan Del Valle*_
+`
+}
+
+exports.help = help
