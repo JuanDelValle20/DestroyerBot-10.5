3b5f563b6d45d72974d7876974866009deac8434
JuanDelValle18 <86022076+JuanDelValle20@users.noreply.github.com>
2022-05-26 20:26:09 -0600

    Update version.js

--- src/version.js
+++ src/version.js
@@ -2,7 +2,7 @@ const version = (prefix, pushname) => {
     return `
     *DestroyerBot ву Juan del Valle*
 
-    *Actualizado:* 08 de Febrero del 2022
+    *Actualizado:* 25 de Mayo del 2022
     *Versión actual:* 10.5
     *Edición ofrecida por:* Juan del Valle™
 
