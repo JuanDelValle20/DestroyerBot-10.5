0aabe72e8aeb216c9e277a8b38c2630707b771ee
JuanDelValle20 <86022076+JuanDelValle20@users.noreply.github.com>
2022-06-24 01:25:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const { exec } = require('child_process');
 let FormData = require('form-data');
 const kagApi = require('@kagchi/kag-api');
 const axios = require("axios");
-let fetch = await import('node-fetch');
+import fetch from 'node-fetch';
 /*const tiktod = require('tiktok-scraper')*/
 const ffmpeg = require('fluent-ffmpeg');
 const { removeBackgroundFromImageFile } = require('remove.bg');
